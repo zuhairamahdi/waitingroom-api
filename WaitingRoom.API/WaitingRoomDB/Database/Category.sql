f2466ad1e82510ca3d1d5ed9785a653c7bdf13b6
Zuhair Abdulla <zuhairamahdi@gmail.com>
2020-08-02 19:21:06 +0300

    Add db project

---
+++ WaitingRoom.API/WaitingRoomDB/Database/Category.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Category]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(250) NULL, 
+    [Initial] NCHAR(1) NULL
+)
---
+++ WaitingRoom.API/WaitingRoomDB/Database/Ticket.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Ticket]
+(
+	[Id] NVARCHAR(50) NOT NULL PRIMARY KEY, 
+    [CeatedAt] DATETIME NULL, 
+    [ProcessedAt] DATETIME NULL
+)
