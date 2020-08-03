34d36ce25c06661fe6b0dbf2f199fa54fd73319d
Zuhair Abdulla <zuhairamahdi@gmail.com>
2020-08-03 11:04:25 +0300

    Add Category Repository

---
+++ WaitingRoom.API/WaitingRoom.API/Repositories/CategoryRepository.cs
@@ -0,0 +1,80 @@
+﻿using DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace WaitingRoom.API.Repositories
+{
+    public class CategoryRepository
+    {
+        public List<Category> GetAll()
+        {
+            using( var contxt = new WaitingRoomDbEntities())
+            {
+                return contxt.Categories.ToList();
+            }
+        }
+        public Category Get(int id)
+        {
+            using (var contxt = new WaitingRoomDbEntities())
+            {
+                var category = contxt.Categories.Where(x => x.Id == id).FirstOrDefault();
+                if (category == null)
+                    throw new Exception($"Category with ID {id} was not found");
+                return category;
+            }
+        }
+        public bool Creat(Category category)
+        {
+            using (var contxt = new WaitingRoomDbEntities())
+            {
+                try
+                {
+                    var newCategory = contxt.Categories.Add(category);
+                    contxt.SaveChanges();
+                    return true;
+                }catch
+                {
+                    return false;
+                }
+            }
+        }
+        public bool Update(int id, Category category)
+        {
+            using (var contxt = new WaitingRoomDbEntities())
+            {
+                try
+                {
+                    var categoryToEdit = Get(id);
+                    categoryToEdit.Initial = category.Initial;
+                    categoryToEdit.Name = category.Name;
+                    contxt.SaveChanges();
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+        public bool Remove(int id)
+        {
+            using (var contxt = new WaitingRoomDbEntities())
+            {
+                try
+                {
+                    var categoryToRemove = Get(id);
+                    contxt.Categories.Remove(categoryToRemove);
+                    contxt.SaveChanges();
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
--- WaitingRoom.API/WaitingRoomDB/Database/Ticket.sql
+++ WaitingRoom.API/WaitingRoomDB/Database/Ticket.sql
@@ -2,5 +2,7 @@
 (
 	[Id] NVARCHAR(50) NOT NULL PRIMARY KEY, 
     [CeatedAt] DATETIME NULL, 
-    [ProcessedAt] DATETIME NULL
+    [ProcessedAt] DATETIME NULL, 
+    [CategoryID] INT NOT NULL, 
+    CONSTRAINT [FK_Ticket_Category] FOREIGN KEY ([CategoryID]) REFERENCES [Category]([Id])
 )
